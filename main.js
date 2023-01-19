fcc613f5a47d6aab34a4ab6fc9a8aee79a1409fb
Gabriele Bianchi <gabriele.bianchi21@gmail.com>
2023-01-19 17:14:19 +0100

    animate light 1.0

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.querySelector('.container-canvas').appendChild( renderer.domElement );
 
-//CUBE 
+//ball -> todo cube
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial( { color: 0xab31ff } );
 const palla = new THREE.Mesh( geometry, material );
@@ -24,11 +24,9 @@ const axisHelp = new THREE.AxesHelper(2);
 scene.add(axisHelp);
 
 //LIGHT
-const light = new THREE.PointLight( 0xff0000, 20, 100 );
+const light = new THREE.PointLight( 0xff0000, 30, 100 );
 light.position.set( 50, 50, 50 );
 scene.add( light );
-
-
 
 window.onresize = function () {
 
@@ -42,6 +40,12 @@ window.onresize = function () {
 function animate() {
     requestAnimationFrame( animate );
 
-    renderer.render( scene, camera );
+    const time = Date.now();
+    const rComp = Math.sin(time * 0.002 + 0.4)  
+    const gComp = Math.sin(time * 0.004 + 0.11) 
+    const bComp = Math.cos(time * 0.001 + 0.13) 
+
+    light.color.setRGB( rComp, gComp, bComp);
+    renderer.render(scene, camera)
 }
 animate();
